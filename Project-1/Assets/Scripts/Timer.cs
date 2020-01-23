487935ca2b334762010b64dba13e7ee1cf4b882f
Stephen Hoffman <sch4240@g.rit.edu>
2020-01-23 18:36:31 -0500

    Timer script code is working
    Is in the scripts folder

---
+++ Project-1/Assets/Scripts/Timer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float seconds;
+    public float startTime = 20;
+    public bool timerTick;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerTick = true;
+        seconds = startTime;
+        GetComponent<Text>().text = "" + seconds;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerTick != false)
+        {
+            seconds -= Time.deltaTime;
+            GetComponent<Text>().text = ""+seconds;
+            if (seconds <= 0)
+            {
+                timerTick = false;
+            }
+        }
+    }
+}
