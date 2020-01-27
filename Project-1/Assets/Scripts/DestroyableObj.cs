cfb7fbef3b82454b8be88303b059732ffa02b14a
Dantayy <nvm6501@rit.edu>
2020-01-27 10:49:38 -0500

    Implemented a visual feedback system for the destroyable object prefab that'll change its color as its health decreases

--- Project-1/Assets/Scripts/DestroyableObj.cs
+++ Project-1/Assets/Scripts/DestroyableObj.cs
@@ -5,29 +5,39 @@ using UnityEngine.SceneManagement;
 
 public class DestroyableObj : MonoBehaviour
 {
-    // variable for object health
-    public int health;
+    // variable for object health at start of game as well as private tracking var
+    public int startHealth;
+    int currentHealth;
+    // color variables to lerp between 
+    Color startColor;
+    [SerializeField]
+    Color damagedColor = Color.red;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // initialize the private vars
+        startColor = GetComponent<Renderer>().material.color;
+        currentHealth = startHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
         // delete this object if its health has reached zero
-       if(health == 0)
+       if(currentHealth == 0)
         {
             Destroy(gameObject);
-        } 
+            return;
+        }
+       // update the color based on the current health of the obj
+        GetComponent<Renderer>().material.color = Color.Lerp(damagedColor, startColor, (float)currentHealth/startHealth);
     }
 
     // handler for clicking on the object, which will deprecate its health
     void OnMouseDown()
     {
-        health--;
-        Debug.Log(health);
+        currentHealth--;
     }
 }
