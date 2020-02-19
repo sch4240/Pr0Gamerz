8ccec7e729f3458f3ab232adb7f10dffaa5fcf74
dantayy <nvm6501@rit.edu>
2020-02-18 20:11:23 -0500

    Added assets for blocks and and background, as well as UI for start, main, and end scenes, and buttons/a script to switch between them (currently not working, not sure why)

---
+++ Project-2/Assets/Scripts/SwitchScenes.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+// Helper function for switching between scenes (to be attached to button OnClick events)
+public class SwitchScenes : MonoBehaviour
+{
+    public void GoToStartScene()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+
+    public void GoToMainScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void GoToEndScene()
+    {
+        SceneManager.LoadScene("EndScene");
+    }
+}
