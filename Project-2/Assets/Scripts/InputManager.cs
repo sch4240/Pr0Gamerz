d7dbbd540e63216ac0baa1a02e80b3f1ca872d18
Ethan Adler (RIT Student) <exa9027@ad.rit.edu>
2020-02-20 16:39:21 -0500

    input system kind of in place

--- Project-2/Assets/Scripts/InputManager.cs
+++ Project-2/Assets/Scripts/InputManager.cs
@@ -6,20 +6,62 @@ using UnityEngine.EventSystems;
 
 public class InputManager : MonoBehaviour
 {
-        // Start is called before the first frame update
-        void Start()
-        {
+    Vector3 lastPos = new Vector3();
+    Vector3 currentPos = new Vector3();
+    float magnitude;
+    bool mousePressed = false;
 
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if(Input.touchCount > 0)
+        {
+            currentPos = Input.GetTouch(0).position;
+            Debug.Log(currentPos);
+        }*/
+        // Debug.Log(Input.GetMouseButton(0));
+        if(Input.GetMouseButton(0))
+        {
+            currentPos = Input.mousePosition;
+            mousePressed = true;
+            // Debug.Log(currentPos);
         }
 
-        // Update is called once per frame
-        void Update()
+        if(mousePressed && Input.GetMouseButton(0) == false)
         {
+            if (lastPos != currentPos)
+            {
+                // Debug.Log(currentPos - lastPos);
+                Vector3 dist = currentPos - lastPos;
+                magnitude = dist.magnitude;
+                Debug.Log(magnitude);
+                lastPos = currentPos;
 
+            }
+            else
+            {
+                magnitude = 0;
+            }
         }
+        
+        // Debug.Log(Input.acceleration);
+
+        // lastPos = currentPos;
+    }
 
     public void OnDrag(BaseEventData eventData)
     {
         
+    }
+
+    public float getMagnitude()
+    {
+        return magnitude;
     }
 }
