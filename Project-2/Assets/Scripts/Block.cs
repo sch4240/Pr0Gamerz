125fda040808decd2c6b9df01b2c9497e2be24d4
Katie Roy <kmr8172@g.rit.edu>
2020-02-20 15:09:53 -0500

    blocks fall down the screen, created prefabs for each kind of block

---
+++ Project-2/Assets/Scripts/Block.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    private Vector3 position;
+
+    // Update is called once per frame
+    void Update()
+    {
+        position = this.transform.position;
+        position.y -= Time.deltaTime * 2;
+        this.transform.position = position;
+        Debug.Log("Position: " + position.y);
+        Debug.Log("transform: " + this.transform.position.y);
+    }
+}
