afcec35819ec79d8f18e581487b6e40a8f32f5fe
Stephen Hoffman <sch4240@g.rit.edu>
2020-02-20 20:51:37 -0500

    Demo Input Implementation

--- Project-2/Assets/Scripts/Block.cs
+++ Project-2/Assets/Scripts/Block.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     private Vector3 position;
+    public string type;
 
     // Update is called once per frame
     void Update()
@@ -12,7 +13,24 @@ public class Block : MonoBehaviour
         position = this.transform.position;
         position.y -= Time.deltaTime * 2;
         this.transform.position = position;
-        Debug.Log("Position: " + position.y);
-        Debug.Log("transform: " + this.transform.position.y);
+        //Debug.Log("Position: " + position.y);
+        //Debug.Log("transform: " + this.transform.position.y);
+    }
+
+    public bool checkType(string swipe)
+    {
+        if(position.y < 5)
+        {
+            if (swipe == type)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return false;
+        
     }
 }
---
+++ Project-2/Assets/Scripts/InputController.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(SwipeManager))]
+public class InputController : MonoBehaviour
+{
+    public Block block;
+    
+    public string direction;
+
+    void Start()
+    {
+        SwipeManager swipeManager = GetComponent<SwipeManager>();
+        swipeManager.onSwipe += HandleSwipe;
+        swipeManager.onLongPress += HandleLongPress;
+    }
+
+    void HandleSwipe(SwipeAction swipeAction)
+    {
+        //Debug.LogFormat("HandleSwipe: {0}", swipeAction);
+        if (swipeAction.direction == SwipeDirection.Up)
+        {
+            direction = "up";
+            // move up
+            if (block != null)
+                if (block.checkType(direction))
+                {
+                    Destroy(gameObject);
+                }
+        }
+        else if (swipeAction.direction == SwipeDirection.UpRight)
+        {
+            direction = "upright";
+            // move right
+            if (block != null)
+                if (block.checkType(direction))
+                {
+                    Destroy(gameObject);
+                }
+        }
+        else if (swipeAction.direction == SwipeDirection.Right)
+        {
+            direction = "right";
+            // move down
+            if (block != null)
+                if (block.checkType(direction))
+                {
+                    Destroy(gameObject);
+                }
+        }
+        else if (swipeAction.direction == SwipeDirection.DownRight)
+        {
+            direction = "downright";
+            // move left
+            if (block != null)
+                if (block.checkType(direction))
+                {
+                    Destroy(gameObject);
+                }
+        }
+        else if (swipeAction.direction == SwipeDirection.Down)
+        {
+            direction = "down";
+            // move left
+            if (block != null)
+                if (block.checkType(direction))
+                {
+                    Destroy(gameObject);
+                }
+        }
+        else if (swipeAction.direction == SwipeDirection.DownLeft)
+        {
+            direction = "downleft";
+            // move left
+            if (block != null)
+                if (block.checkType(direction))
+                {
+                    Destroy(gameObject);
+                }
+        }
+        else if (swipeAction.direction == SwipeDirection.Left)
+        {
+            direction = "left";
+            // move left
+            if (block != null)
+                if (block.checkType(direction))
+                {
+                    Destroy(gameObject);
+                }
+        }
+        else if (swipeAction.direction == SwipeDirection.UpRight)
+        {
+            direction = "upright";
+            // move left
+            if (block != null)
+                if (block.checkType(direction))
+                {
+                    Destroy(gameObject);
+                }
+        }
+    }
+
+    void HandleLongPress(SwipeAction swipeAction)
+    {
+        //Debug.LogFormat("HandleLongPress: {0}", swipeAction);
+    }
+}
