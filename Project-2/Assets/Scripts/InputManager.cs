09c65406ddae8807e48e12fe8060e51671048d3e
Ethan Adler (RIT Student) <exa9027@ad.rit.edu>
2020-03-02 10:53:30 -0500

    dot spawning started

--- Project-2/Assets/Scripts/InputManager.cs
+++ Project-2/Assets/Scripts/InputManager.cs
@@ -6,17 +6,21 @@ using UnityEngine.EventSystems;
 
 public class InputManager : MonoBehaviour
 {
+    public GameObject dot;
         // Start is called before the first frame update
-        void Start()
-        {
+    void Start()
+    {
 
-        }
+    }
 
         // Update is called once per frame
-        void Update()
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
         {
-
+            Instantiate(dot, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
         }
+    }
 
     public void OnDrag(BaseEventData eventData)
     {
