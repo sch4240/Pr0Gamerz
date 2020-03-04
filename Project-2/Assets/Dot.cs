a093dd61ee86d795f5298d92b330f7884feec7f7
Ethan Adler (RIT Student) <exa9027@ad.rit.edu>
2020-03-04 10:38:44 -0500

    trail fades-ish

--- Project-2/Assets/Dot.cs
+++ Project-2/Assets/Dot.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Dot : MonoBehaviour
 {
+    float totalTime = 0.5f;
     float timeLeft = 0.5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -18,6 +21,10 @@ public class Dot : MonoBehaviour
         if (timeLeft < 0)
         {
             GameObject.Destroy(gameObject);
+        }
+        else
+        {
+            gameObject.GetComponent<Image>().color = new Color(255, 255, 255, 255 * (timeLeft / totalTime));
         }
     }
 }
