f11d3af425056b8a149d4cb4bd7a665dc25e8f90
Stephen Hoffman (RIT Student) <sch4240@ad.rit.edu>
2020-03-05 00:44:17 -0500

    Beatmapping works

---
+++ Project-2/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public List<Block> blocks;
+
+    float beatTimer = 2.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        beatTimer -= Time.deltaTime;
+
+        if(beatTimer < 0)
+        {
+            int index = Random.Range(0, blocks.Count);
+            Vector2 blockPos = new Vector2(Random.Range(-215, 213), 354);
+            Block block = Instantiate(blocks[index], blockPos, Quaternion.identity);
+            block.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, false);
+            beatTimer = 2.0f;
+        }
+    }
+}
